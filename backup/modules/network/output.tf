12d0cda87f2b964fc4145a50a0df7797076689e8
Kevin McGarry <kevinjmcgarry@gmail.com>
2019-11-12 10:24:07 -0600

    removed backup folder

--- backup/modules/network/output.tf
+++
@@ -1,8 +0,0 @@
-# output vpc_id to be used in the security module
-output "vpc_id" {
-  value = "${aws_vpc.terraform-eks-demo.id}"
-}
-
-output "app_subnet_ids" {
-  value = "${aws_subnet.application.*.id}"
-}
